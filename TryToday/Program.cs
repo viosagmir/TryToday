511df181d1896a3238928fe78e927b9f439d6bb5
viosagmir <viosagmir@gmail.com>
2024-03-02 10:07:02 +0300

    Init

---
+++ TryToday/Program.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Add Comment
+        var botClient = new TelegramBotClient("7090445360:AAG0dIyE55BrG7jIz5f83a1soox4waHnlYU");
+
+        botClient.StartReceiving(Update, Error);
+
+        Console.ReadLine();
+    }
+
+    private static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+    {
+        var message = update.Message;
+
+        await botClient.SendTextMessageAsync(message.Chat.Id, "Hello");
+
+        return;
+    }
+
+    private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
